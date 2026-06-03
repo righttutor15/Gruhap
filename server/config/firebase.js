5c643dac40bf55537cc24891ede386c126bd5f49
akshay kumar <akshay.1904kumar@gmail.com>
2026-06-03 15:49:18 +0530

    chore: update project dependencies and generate lockfile

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,5 +1,10 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../gruhap-a7354-firebase-adminsdk-fbsvc-9023e72727.json');
+let serviceAccount;
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    serviceAccount = require('../gruhap-a7354-firebase-adminsdk-fbsvc-9023e72727.json');
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
